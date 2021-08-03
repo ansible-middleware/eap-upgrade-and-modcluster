6a2e3a2bf3358ffdf3279b8acf23e9e8075b6005
Romain Pelisse <belaran@gmail.com>
2021-08-03 18:31:04 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
+export ANSIBLE_KEEP_REMOTE_FILES=1
 
 readonly JCLIFF_INSTALL_DIR=${JCLIFF_INSTALL_DIR:-'/root/jcliff.git'}
 readonly JCLIFF_GIT_URL=${JCLIFF_GIT_URL:-'https://github.com/wildfly-extras/ansible_collections_jcliff.git'}
 
-#subscription-manager refresh
-yum install -y git
-
-git clone "${JCLIFF_GIT_URL}" "${JCLIFF_INSTALL_DIR}"
-cd "${JCLIFF_INSTALL_DIR}"
-ansible-galaxy collection build .
-ansible-galaxy collection install *.tar.gz
-cd -
-
-ansible-galaxy install sabre1041.redhat-csp-download
+subscription-manager refresh
+ansible-galaxy collection install middleware_automation.redhat_csp_download
+ansible-galaxy collection install artefacts/middleware_automation-jcliff-0.0.6.tar.gz
+ansible-playbook -i inventory -e @artefacts/creds.yml playbook.yml
